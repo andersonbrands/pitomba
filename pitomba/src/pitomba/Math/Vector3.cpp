797aba7c6b6e1a4a8d6ec1ea4ecb11e7e8e92590
Anderson Brandao <anderson.brands@gmail.com>
2021-05-15 20:11:12 -0300

    Add Vector3

---
+++ pitomba/src/pitomba/Math/Vector3.cpp
@@ -0,0 +1,186 @@
+
+#include "Vector3.h"
+
+namespace pitomba {
+
+    Vector3::Vector3(const float x, const float y, const float z) :
+        x_(x), y_(y), z_(z) {}
+
+    Vector3::Vector3(const float value) :
+        x_(value), y_(value), z_(value) {}
+
+    Vector3& Vector3::operator=(const Vector3& in) {
+        x_ = in.getX();
+        y_ = in.getY();
+        z_ = in.getZ();
+        return *this;
+    }
+
+    // getters
+    float Vector3::getX() const {
+        return x_;
+    }
+    float Vector3::getY() const {
+        return y_;
+    }
+    float Vector3::getZ() const {
+        return z_;
+    }
+
+    // setters
+    void Vector3::setX(const float x) {
+        x_ = x;
+    }
+    void Vector3::setY(const float y) {
+        y_ = y;
+    }
+    void Vector3::setZ(const float z) {
+        z_ = z;
+    }
+
+    void Vector3::set(float value) {
+        x_ = y_ = z_ = value;
+    }
+    void Vector3::set(float x, float y, float z) {
+        x_ = x;
+        y_ = y;
+        z_ = z;
+    }
+
+    // operators
+    Vector3 Vector3::operator +(const Vector3& rhs) const {
+        return Vector3(x_ + rhs.x_, y_ + rhs.y_, z_ + rhs.z_);
+    }
+    Vector3 Vector3::operator -(const Vector3& rhs) const {
+        return Vector3(x_ - rhs.x_, y_ - rhs.y_, z_ - rhs.z_);
+    }
+    Vector3 Vector3::operator *(const Vector3& rhs) const {
+        return Vector3(x_ * rhs.x_, y_ * rhs.y_, z_ * rhs.z_);
+    }
+    Vector3 Vector3::operator /(const Vector3& rhs) const {
+        return Vector3(x_ / rhs.x_, y_ / rhs.y_, z_ / rhs.z_);
+    }
+
+    Vector3 Vector3::operator +(const float scalar) const {
+        return Vector3(x_ + scalar, y_ + scalar, z_ + scalar);
+    }
+    Vector3 Vector3::operator -(const float scalar) const {
+        return Vector3(x_ - scalar, y_ - scalar, z_ - scalar);
+    }
+    Vector3 Vector3::operator *(const float scalar) const {
+        return Vector3(x_ * scalar, y_ * scalar, z_ * scalar);
+    }
+    Vector3 Vector3::operator /(const float scalar) const {
+        return Vector3(x_ / scalar, y_ / scalar, z_ / scalar);
+    }
+
+    Vector3& Vector3::operator +=(const Vector3& rhs) {
+        x_ += rhs.x_;
+        y_ += rhs.y_;
+        z_ += rhs.z_;
+
+        return *this;
+    }
+    Vector3& Vector3::operator -=(const Vector3& rhs) {
+        x_ -= rhs.x_;
+        y_ -= rhs.y_;
+        z_ -= rhs.z_;
+
+        return *this;
+    }
+    Vector3& Vector3::operator *=(const Vector3& rhs) {
+        x_ *= rhs.x_;
+        y_ *= rhs.y_;
+        z_ *= rhs.z_;
+
+        return *this;
+    }
+    Vector3& Vector3::operator /=(const Vector3& rhs) {
+        x_ /= rhs.x_;
+        y_ /= rhs.y_;
+        z_ /= rhs.z_;
+
+        return *this;
+    }
+
+    Vector3& Vector3::operator +=(const float scalar) {
+        x_ += scalar;
+        y_ += scalar;
+        z_ += scalar;
+
+        return *this;
+    }
+    Vector3& Vector3::operator -=(const float scalar) {
+        x_ -= scalar;
+        y_ -= scalar;
+        z_ -= scalar;
+
+        return *this;
+    }
+    Vector3& Vector3::operator *=(const float scalar) {
+        x_ *= scalar;
+        y_ *= scalar;
+        z_ *= scalar;
+
+        return *this;
+    }
+    Vector3& Vector3::operator /=(const float scalar) {
+        x_ /= scalar;
+        y_ /= scalar;
+        z_ /= scalar;
+
+        return *this;
+    }
+
+    Vector3 Vector3::operator -() const {
+        return Vector3(-x_, -y_, -z_);
+    }
+
+    bool Vector3::operator ==(const Vector3& rhs) const {
+        if (x_ == rhs.x_ && y_ == rhs.y_ && z_ == rhs.z_)
+            return true;
+        else
+            return false;
+    }
+    bool Vector3::operator !=(const Vector3& rhs) const {
+        return !(*this == rhs);
+    }
+
+    // vector operations
+    Vector3 Vector3::cross(const Vector3& rhs) const {
+        return Vector3(y_ * rhs.z_ - z_ * rhs.y_, z_ * rhs.x_ - x_ * rhs.z_, x_ * rhs.y_ - y_ * rhs.x_);
+    }
+
+    float Vector3::dot(const Vector3& rhs) const {
+        return (x_ * rhs.x_ + y_ * rhs.y_ + z_ * rhs.z_);
+    }
+
+    float Vector3::lengthSquared() const {
+        return (x_ * x_ + y_ * y_ + z_ * z_);
+    }
+
+    float Vector3::length() const {
+        return sqrt(lengthSquared());
+    }
+
+    void Vector3::normalise() {
+        float len(length());
+
+        if (len != 0.0f) {
+            *this /= len;
+        }
+    }
+
+    float Vector3::distance(const Vector3& v) const {
+        return sqrt(distanceSquared(v));
+    }
+
+    float Vector3::distanceSquared(const Vector3& v) const {
+        float dX(x_ - v.x_);
+        float dY(y_ - v.y_);
+
+        return dX * dX + dY * dY;
+    }
+
+
+}
---
+++ pitomba/src/pitomba/Math/Vector3.h
@@ -0,0 +1,76 @@
+
+#ifndef VECTOR3_H_
+#define VECTOR3_H_
+
+#include <cmath>
+
+namespace pitomba {
+
+    class Vector3 {
+    private:
+        float x_ = 0.0F;
+        float y_ = 0.0F;
+        float z_ = 0.0F;
+
+    public:
+        Vector3() = default;
+        Vector3(const float x, const float y, const float z);
+        explicit Vector3(const float value);
+
+        virtual ~Vector3() = default;
+
+        Vector3& operator=(const Vector3& in);
+
+        // getters
+        float getX() const;
+        float getY() const;
+        float getZ() const;
+
+        // setters
+        void setX(const float x);
+        void setY(const float y);
+        void setZ(const float z);
+        void set(float value);
+        void set(float x, float y, float z);
+
+        // operators
+        Vector3 operator +(const Vector3& rhs) const;
+        Vector3 operator -(const Vector3& rhs) const;
+        Vector3 operator *(const Vector3& rhs) const;
+        Vector3 operator /(const Vector3& rhs) const;
+
+        Vector3 operator +(const float scalar) const;
+        Vector3 operator -(const float scalar) const;
+        Vector3 operator *(const float scalar) const;
+        Vector3 operator /(const float scalar) const;
+
+        Vector3& operator +=(const Vector3& rhs);
+        Vector3& operator -=(const Vector3& rhs);
+        Vector3& operator *=(const Vector3& rhs);
+        Vector3& operator /=(const Vector3& rhs);
+
+        Vector3& operator +=(const float scalar);
+        Vector3& operator -=(const float scalar);
+        Vector3& operator *=(const float scalar);
+        Vector3& operator /=(const float scalar);
+
+        Vector3 operator -() const;
+
+        bool operator ==(const Vector3& rhs) const;
+        bool operator !=(const Vector3& rhs) const;
+
+        // vector operations
+        Vector3 cross(const Vector3& rhs) const;
+        float dot(const Vector3& rhs) const;
+        float lengthSquared() const;
+        float length() const;
+        void normalise();
+
+        // additional operations
+        float distance(const Vector3& v) const;
+        float distanceSquared(const Vector3& v) const;
+    };
+
+}
+
+#endif // VECTOR3_H_
