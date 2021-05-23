6633386f73faf23fdfc158fe445a762a395abe1b
Anderson Brandao <anderson.brands@gmail.com>
2021-05-23 01:29:29 -0300

    Add sample texture

---
+++ pitomba/src/Sample/Ids/TextureIds.h
@@ -0,0 +1,14 @@
+
+#ifndef TEXTURE_IDS_H_
+#define TEXTURE_IDS_H_
+
+#include "../../pitomba/Utils/Utils.h"
+
+using namespace pitomba;
+
+namespace texture {
+    //static const IdAndName TEXTURE_NAME = { id, L"name" };
+    static const IdAndName SAMPLE_TEXTURE = { 0, L"sample.png" };
+}
+
+#endif // TEXTURE_IDS_H_
